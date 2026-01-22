3a4b9aec1b6003666d4d82261669eec411435ac2
NotAI <notai@notai.xyz>
2026-01-21 22:32:11 -0500

    Initialized site

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [svelte()]
+});
