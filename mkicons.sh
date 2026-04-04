597127ca169d7adf8328f7fd8813e17dc337f0b7
NotAI <notai@notai.xyz>
2026-04-04 02:59:57 -0400

    Added a new 180 sized mode for optimal apple touch icon

--- mkicons.sh
+++ mkicons.sh
@@ -8,6 +8,7 @@ FAVICON_SQUARE="public/favicon-square.svg"
 magick -background none "${FAVICON}" -define icon:auto-resize=16,32,48 'public/favicon.ico'
 
 # Create icons if page is saved to mobile screens
+magick -background none -size 180x180 "${FAVICON_SQUARE}" 'public/thayen-dev-180.png'
 magick -background none -size 192x192 "${FAVICON_SQUARE}" 'public/thayen-dev-192.png'
 magick -background none -size 384x384 "${FAVICON_SQUARE}" 'public/thayen-dev-384.png'
 magick -background none -size 512x512 "${FAVICON_SQUARE}" 'public/thayen-dev-512.png'
