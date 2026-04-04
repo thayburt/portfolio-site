0e18ba25267a10031899ef37cd81335d87494dfd
NotAI <notai@notai.xyz>
2026-04-04 04:40:49 -0400

    Fixed site, removed unnecessary adapter and reverted to properly building a static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 
-import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
@@ -9,9 +8,5 @@ import assetGen from "./generate-assets";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://thayen.dev",
-	adapter: cloudflare({
-		prerenderEnvironment: "node",
-	}),
-	integrations: [icon(), sitemap(), assetGen()],
-	trailingSlash: "always",
+	integrations: [icon(), sitemap(), assetGen()]
 });
