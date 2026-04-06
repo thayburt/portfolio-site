b669f12f851035007cee85cc9e3dcb3706d3c502
NotAI <notai@notai.xyz>
2026-04-05 22:17:16 -0400

    Formatted project

--- generate-assets.ts
+++ generate-assets.ts
@@ -1,14 +1,14 @@
 // image-gen-integration.js
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
 
 export default function generateAssets() {
-  return {
-    name: 'generate-assets',
-    hooks: {
-      'astro:config:setup': () => {
-        console.log('Generating images from SVGs...');
-        execSync('./scripts/mkicons.sh', { stdio: 'inherit' });
-      },
-    },
-  };
+	return {
+		name: "generate-assets",
+		hooks: {
+			"astro:config:setup": () => {
+				console.log("Generating images from SVGs...");
+				execSync("./scripts/mkicons.sh", { stdio: "inherit" });
+			},
+		},
+	};
 }
